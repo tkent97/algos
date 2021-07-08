d64b2de77eb8880b8433b5904300b78f3b2d395c
tkent97 <tristen123abc@gmail.com>
2021-07-08 09:06:26 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/* 
+    String: Reverse
+    Given a string,
+    return a new string that is the given string reversed
+
+    
+    var expected1 = "erutaerc";
+
+    var str2 = "dog";
+    var expected2 = "god";
+
+
+    * Reverses the given str.
+    * - Time: O(?).
+    * - Space: O(?).
+    * @param {string} str String to be reversed.
+    * @returns {string} The given str reversed.
+*/
+var str1 = "creature";
+function reverseString(str) {
+var temp = "";
+var newlist = [];
+for(i = 0; i<=str1.length - 1; i++)
+    temp += str1[i]; 
+    console.log(temp);
+}
+
+reverseString(str1);
